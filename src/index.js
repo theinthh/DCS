e41e4f12cec31b7a301d23630fd3060de0e6c77c
theinthh <46649953+theinthh@users.noreply.github.com>
2020-02-26 01:15:13 +0630

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-
+import { BrowserRouter as Router } from "react-router-dom";
 //ReactDOM.render(<Years />, document.getElementById("root"));
-ReactDOM.render(<App/>, document.getElementById("root"));
-//ReactDOM.render(<StudentInfo />, document.getElementById("root"));
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+); //ReactDOM.render(<StudentInfo />, document.getElementById("root"));
